0bcbeabe4d6ff887f6537d4a950f6e1299755b72
Artem Bachevsky <fryday.kg@gmail.com>
2015-11-14 17:35:50 +0300

    Some  fixes

--- src/main.py
+++ src/main.py
@@ -10,18 +10,20 @@ class MainPage(webapp2.RequestHandler):
 	def get(self):
 		user = users.get_current_user()
 		if user:
+			self.response.write('<html><body>')
 			self.response.write('<a href="tags/"> My tags </a>')
 			self.response.write('<br><br>')
 			
 			self.response.write("""
-<script src="js/tagProvider.js"/>
+<script src="js/tagManager.js"></script>
 
 <input type="text" class="js-copytextarea" id="tag" value="" size=45 readonly/>
 <input type="button" onclick="getTag()" value="Get tag"/>
 <input type="button" class="js-textareacopybtn" value="Copy"/>
 
-<script src="js/clipboardCopy.js"/>
+<script src="js/clipboardCopy.js"></script>
 """)
+			self.response.write('</body></html>')
 		else:
 			self.redirect(users.create_login_url(self.request.uri))
 
